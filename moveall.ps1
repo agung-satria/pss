6a9923135bab10d86e984e2d66357d8b6f8d462f
Agung Satria <80804186+agung-satria@users.noreply.github.com>
2022-05-20 09:38:28 +0700

    Update moveall.ps1

--- moveall.ps1
+++ moveall.ps1
@@ -1,4 +1,12 @@
+echo "Moving all file"
+echo "From:"
+echo "$dari"
+echo "To:"
+echo "$ke"
+$dari = "D:\Agung Satria\jawaban-uts-database"
+$ke = "D:\Agung Satria\jawaban-uts-database-send"
+
 for (;;) {
   start-sleep -s 2
-  move-item 'D:\Agung Satria\jawaban\*' 'D:\Agung Satria\jawaban-send' -ErrorAction SilentlyContinue -force
+  move-item $dari/* $ke -ErrorAction SilentlyContinue -force
 }
